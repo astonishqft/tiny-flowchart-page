e4726fa2aaca915e30dc43c73013d68cfe6236ec
astonishqft <1551501581@qq.com>
2025-10-05 11:56:31 +0800

    fix: 路径兼容

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import DemoView from '../views/DemoView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL || '/'),
   routes: [
     {
       path: '/',
